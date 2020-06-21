0af20113ec604d23c442fef1f15a0f1ed56d3110
Matt Mullins <mmullins@mmlx.us>
2020-06-20 20:59:03 -0700

    led_loop: turn the system clock up to 168MHz
    As far up as the system will go!  This gets me down to about a 300ns
    period for the LED cycle.

--- src/bin/led_loop.rs
+++ src/bin/led_loop.rs
@@ -5,13 +5,15 @@
 use panic_itm as _;
 use stm32f407g_disc::entry;
 
-use stm32f407g_disc::hal::gpio::GpioExt;
+use stm32f407g_disc::hal::prelude::*;
 use stm32f407g_disc::led::LedColor::Orange;
-
 
 #[entry]
 fn main() -> ! {
     let peripherals = stm32f407g_disc::Peripherals::take().unwrap();
+
+    let rcc = peripherals.RCC.constrain();
+    let clocks = rcc.cfgr.use_hse(8.mhz()).sysclk(168.mhz()).freeze();
 
     let gpiod = peripherals.GPIOD.split();
     let mut leds = stm32f407g_disc::led::Leds::new(gpiod);
