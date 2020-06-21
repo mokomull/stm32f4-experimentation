b4a5559c671ca7a15e71f900c3019c071e37b88b
Matt Mullins <mmullins@mmlx.us>
2020-06-21 14:35:59 -0700

    led_loop: test time it takes to change direction
    This measured about 100ns, bringing the total cost to 310ns.

--- src/bin/led_loop.rs
+++ src/bin/led_loop.rs
@@ -6,10 +6,10 @@ use stm32f407g_disc::entry;
 
 use stm32f407g_disc::hal::prelude::*;
 
-use stm32f407g_disc::hal::gpio::{gpiod::PD13, Output, PushPull};
+use stm32f407g_disc::hal::gpio::{gpiod::PD13, Floating, Input};
 use stm32f407g_disc::hal::interrupt;
 
-static mut LED: *mut PD13<Output<PushPull>> = 0 as *mut _;
+static mut LED: *mut PD13<Input<Floating>> = 0 as *mut _;
 
 #[entry]
 fn main() -> ! {
@@ -20,7 +20,7 @@ fn main() -> ! {
     let clocks = rcc.cfgr.use_hse(8.mhz()).sysclk(168.mhz()).freeze();
 
     let gpiod = peripherals.GPIOD.split();
-    let pin = gpiod.pd13.into_push_pull_output();
+    let pin = gpiod.pd13.into_floating_input();
 
     unsafe {
         LED = &pin as *const _ as *mut _;
@@ -42,7 +42,8 @@ fn main() -> ! {
 
 #[interrupt]
 fn EXTI0() {
-    let pin = unsafe { &mut *LED };
+    let pin = unsafe { core::ptr::read(LED) };
+    let mut pin = pin.into_push_pull_output();
     pin.set_high().unwrap();
 
     let exti = unsafe { &*stm32f407g_disc::stm32::EXTI::ptr() };
