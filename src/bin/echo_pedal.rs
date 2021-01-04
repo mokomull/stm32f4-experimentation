b7315e011dd3ad4deeb3412e7bd10f6599cc8174
Matt Mullins <mmullins@mmlx.us>
2021-01-03 20:00:47 -0800

    echo pedal: replace STM32 DAC with the CS43L22
    This pretty much yanked most of the code from the original usage in the
    early days of the signal generator, and reverts the ADC buffer to being
    (a) not averaged, and (b) handled in a busy loop, so I don't have to
    worry about differences between the clock domains -- it gets slightly
    less than 48kHz for the sample clock since MCLK has to be 256x the
    sample rate.
    
    This does result in the ADC-captured waveform appearing at the DAC,
    though attenuated by a factor of about 12.

--- src/bin/echo_pedal.rs
+++ src/bin/echo_pedal.rs
@@ -8,17 +8,14 @@ use stm32f4xx_hal::prelude::*;
 
 use stm32f4xx_hal::stm32;
 
-use biquad::Biquad;
+use stm32::spi1::i2scfgr;
 
-// 84MHz, since I suppose the APBx prescaler causes the timer clock to be doubled...
-const TIMER_CLOCK_RATE: usize = 84_000_000;
+// approximate!  This will actually run a fraction of a percent slow, due to I2S clocking
+// constraints.
 const SAMPLE_RATE: usize = 48_000;
-const SAMPLES_TO_AVERAGE: usize = 10;
-const TIMER_RATE: usize = SAMPLE_RATE * SAMPLES_TO_AVERAGE;
-// the timer won't behave correctly if the sample rate is not an exact integer number of ticks
-static_assertions::const_assert_eq!(TIMER_CLOCK_RATE % TIMER_RATE, 0);
-// nor if it takes more than 32 bits to represent the delay
-static_assertions::const_assert!(TIMER_CLOCK_RATE / TIMER_RATE <= 0xFFFF_FFFF);
+
+// fixed address of the DAC on the I2C bus
+const ADDRESS: u8 = 0x94 >> 1;
 
 #[entry]
 fn main() -> ! {
@@ -26,23 +23,39 @@ fn main() -> ! {
     let mut core_peripherals = cortex_m::Peripherals::take().unwrap();
 
     let rcc = peripherals.RCC.constrain();
-    let _clocks = rcc.cfgr.use_hse(8.mhz()).sysclk(168.mhz()).freeze();
+    let clocks = rcc.cfgr.use_hse(8.mhz()).sysclk(168.mhz()).freeze();
 
     let _itm = &mut core_peripherals.ITM.stim[0];
 
     let porta = peripherals.GPIOA.split();
+    let portb = peripherals.GPIOB.split();
+    let portc = peripherals.GPIOC.split();
+    let portd = peripherals.GPIOD.split();
 
-    // the DAC overrides what was selected in the GPIO module, but the datasheet recommended the pin
-    // be switched to analog input.
-    let _signal_out = porta.pa4.into_analog();
     let _signal_in = porta.pa0.into_analog();
+
+    let _mck = portc.pc7.into_alternate_af6();
+    let _sck = portc.pc10.into_alternate_af6();
+    let _sd = portc.pc12.into_alternate_af6();
+    let _ws = porta.pa4.into_alternate_af6();
 
     // enable the DAC peripheral
     unsafe {
         let rcc = &*stm32::RCC::ptr();
+
+        // enable the I2S PLL: the VCO input should be 2MHz since we have an 8MHz crystal -- but that's
+        // going to depend on what freeze() chose above.
+        // 2MHz * 129 / 3 = 86 MHz
+        rcc.plli2scfgr.write(|w| {
+            w.plli2sn().bits(129);
+            w.plli2sr().bits(3)
+        });
+        rcc.cr.modify(|_r, w| w.plli2son().set_bit());
+        while !rcc.cr.read().plli2srdy().bit() {}
+
         rcc.apb1enr.modify(|_r, w| {
             w.dacen().set_bit();
-            w.tim2en().set_bit()
+            w.spi3en().set_bit()
         });
         rcc.ahb1enr.modify(|_r, w| {
             w.dma1en().set_bit();
@@ -51,69 +64,55 @@ fn main() -> ! {
         rcc.apb2enr.modify(|_r, w| w.adc1en().set_bit())
     }
 
-    let coeffs = biquad::Coefficients::<f32>::from_params(
-        biquad::Type::LowPass,
-        biquad::Hertz::<f32>::from_hz(TIMER_RATE as f32).unwrap(),
-        biquad::Hertz::<f32>::from_hz(20_000.0).unwrap(),
-        biquad::Q_BUTTERWORTH_F32,
-    )
-    .unwrap();
-    let mut filter = [biquad::DirectForm2Transposed::<f32>::new(coeffs); 6];
+    let spi = peripherals.SPI3;
+    spi.i2scfgr.write(|w| {
+        w.i2smod().set_bit();
+        w.i2scfg().variant(i2scfgr::I2SCFG_A::MASTERTX);
+        w.i2sstd().variant(i2scfgr::I2SSTD_A::MSB);
+        w.ckpol().set_bit();
+        w.datlen().variant(i2scfgr::DATLEN_A::SIXTEENBIT);
+        w.chlen().variant(i2scfgr::CHLEN_A::SIXTEENBIT)
+    });
+    // 86MHz / (3 * 2 + 1) = 12.2857MHz MCK
+    // 12.2857MHz / 8 [fixed in hardware] = 1.53571MHz bit clock
+    // 1.53571MHz / (2 channels * 16 bits per sample) = 47.9911k samples per sec
+    spi.i2spr.write(|w| {
+        w.mckoe().set_bit();
+        unsafe { w.i2sdiv().bits(3) };
+        w.odd().set_bit()
+    });
+    spi.i2scfgr.modify(|_r, w| w.i2se().set_bit());
 
-    let dma2 = peripherals.DMA2;
-    let timer = peripherals.TIM2;
+    let mut audio_reset = portd.pd4.into_push_pull_output();
+    audio_reset.set_high().unwrap();
+    let mut i2c = stm32f4xx_hal::i2c::I2c::i2c1(
+        peripherals.I2C1,
+        (
+            portb.pb6.into_alternate_af4_open_drain(),
+            portb.pb9.into_alternate_af4_open_drain(),
+        ),
+        50.khz(),
+        clocks,
+    );
+
+    set_dac_register(&mut i2c, 0x04, 0xaf); // headphone channels ON, speaker channels OFF
+    set_dac_register(&mut i2c, 0x05, 0x80); // auto = 1, everything else 0
+    set_dac_register(&mut i2c, 0x06, 0x00); // I2S slave, not inverted, not DSP mode, left justified format
+    set_dac_register(&mut i2c, 0x07, 0x00); // leave Interface Control 2 alone
+
+    // section 4.11 from the CS43L22 datasheet
+    set_dac_register(&mut i2c, 0x00, 0x99);
+    set_dac_register(&mut i2c, 0x47, 0x80);
+    set_dac_register(&mut i2c, 0x32, 0x80);
+    set_dac_register(&mut i2c, 0x32, 0x00);
+
+    // step 6 of 4.9 of CS43L22 datasheet
+    set_dac_register(&mut i2c, 0x02, 0x9e);
+
     let adc = peripherals.ADC1;
-    let dac = peripherals.DAC;
 
     // buffer that can hold a second of data
     let mut buffer = [0u16; SAMPLE_RATE];
-    // and ones that can hold just enough from the ADC, to ping-pong between
-    let mut adc_buffer = [[0u16; SAMPLES_TO_AVERAGE]; 2];
-
-    // set up DMA2 to read from ADC1 into buffer, in a circular fashion
-    let adc_stream = &dma2.st[0];
-    // from the ADC
-    adc_stream
-        .par
-        .write(|w| unsafe { w.bits(&adc.dr as *const _ as u32) });
-    // to buffer
-    adc_stream
-        .m0ar
-        .write(|w| unsafe { w.bits(&mut adc_buffer[0][0] as *mut _ as u32) });
-    // how many samples
-    adc_stream
-        .ndtr
-        .write(|w| w.ndt().bits(SAMPLES_TO_AVERAGE as u16));
-    // and let 'er rip!
-    adc_stream.cr.write(|w| {
-        w.chsel().bits(0);
-        // everything is a single sample at a time
-        w.mburst().single();
-        w.pburst().single();
-        // we'll be managing our own buffers, not hardware double-buffered
-        w.dbm().disabled();
-        // 16 bits at a time
-        w.msize().bits16();
-        w.psize().bits16();
-        // increment memory address, but read from the ADC every time
-        w.minc().incremented();
-        w.pinc().fixed();
-        // again, we'll be managing this ourselves
-        w.circ().disabled();
-        // we're reading from the ADC
-        w.dir().peripheral_to_memory();
-        // the DMA controller knows how many bytes to transfer
-        w.pfctrl().dma();
-        // finally: enable the DMA stream!
-        w.en().enabled()
-    });
-
-    // subtract one because the timer iterates from zero through (and including) this value.
-    timer
-        .arr
-        .write(|w| w.arr().bits((TIMER_CLOCK_RATE / TIMER_RATE - 1) as u32));
-    timer.cr2.write(|w| w.mms().update()); // send a TRGO event when the timer updates
-    timer.cr1.write(|w| w.cen().set_bit());
 
     // run the ADC on TIM2_TRGO (that is: at TIMER_RATE samples/sec) and have it request DMA.
     // only want one measurement each clock, from channel 0
@@ -123,68 +122,45 @@ fn main() -> ! {
     // run the ADC clock at pclk2 (84MHz) / 8 = 10.5MHz.  That's now within the datasheet tolerance
     // for the ADC, and plenty fast to take one sample 48k times per second.
     peripherals.ADC_COMMON.ccr.modify(|_r, w| w.adcpre().div8());
-    // set up the ADC for 12-bit samples, DMA
+    // set up the ADC for 12-bit samples, continuous
     adc.cr1.write(|w| w.res().twelve_bit());
     adc.cr2.write(|w| {
-        w.exten().rising_edge();
-        w.extsel().tim2trgo();
+        w.exten().disabled();
         w.align().right();
-        w.dds().continuous();
-        w.dma().enabled();
+        w.dma().disabled();
+        w.cont().continuous();
         w.adon().enabled()
     });
-
-    // enable the DAC
-    dac.cr.write(|w| w.en1().set_bit());
+    // and kick off the ADC
+    adc.cr2.modify(|_r, w| w.swstart().start());
 
     for i in (0..buffer.len()).cycle() {
-        let (this, next) = (i % 2, (i + 1) % 2);
+        // wait for the SPI peripheral to need another sample
+        while !spi.sr.read().txe().bit() {}
 
-        // wait for the DMA buffer to fill up
-        while adc_stream.cr.read().en().bit() {}
+        // give it another sample
+        spi.dr.write(|w| w.dr().bits(buffer[i]));
 
-        // set up DMA to the next adc_buffer
-        adc_stream
-            .m0ar
-            .write(|w| w.m0a().bits(&adc_buffer[next][0] as *const _ as u32));
-        adc_stream
-            .ndtr
-            .write(|w| w.ndt().bits(SAMPLES_TO_AVERAGE as u16));
-        dma2.lifcr.write(|w| {
-            w.ctcif0().set_bit();
-            w.chtif0().set_bit();
-            w.cteif0().set_bit();
-            w.cdmeif0().set_bit();
-            w.cfeif0().set_bit()
-        });
-        adc_stream.cr.modify(|_r, w| w.en().enabled());
-        // clear the "end of conversion" flag so that the next iteration knows to wait
-        adc.sr.modify(|_r, w| w.eoc().clear_bit());
-        // in order to reset the "DMA completed" status, you have to turn DMA off
-        adc.cr2.modify(|_r, w| w.dma().disabled());
-        // and back on again
-        adc.cr2.modify(|_r, w| w.dma().enabled());
+        // and while we're waiting for that to get clocked-out, grab the current value from the ADC
+        let new_sample = adc.dr.read().data().bits();
 
-        let mut new_sample = 0.0;
-        for i in &adc_buffer[this] {
-            new_sample = *i as f32 / 4096.0;
-            for f in &mut filter {
-                new_sample = f.run(new_sample);
-            }
-        }
+        // wait for the SPI peripheral to need the same sample for the other channel
+        while !spi.sr.read().txe().bit() {}
+        spi.dr.write(|w| w.dr().bits(buffer[i]));
 
         // write that sample 800ms into the future
-        buffer[(i + (SAMPLE_RATE * 800 / 1000)) % buffer.len()] = (new_sample * 4096.0) as u16;
-
-        // output to the DAC
-        dac.dhr12r1
-            .write(|w| unsafe { w.dacc1dhr().bits(buffer[i]) });
-
-        // for sanity sake, make sure we didn't take too long computing the average
-        if adc.sr.read().ovr().bit() {
-            panic!("overran the ADC");
-        }
+        buffer[(i + (SAMPLE_RATE * 800 / 1000)) % buffer.len()] = new_sample;
     }
 
     panic!("cycle() should never complete");
+}
+
+use embedded_hal::blocking::i2c;
+
+fn set_dac_register<I>(i2c: &mut I, register: u8, value: u8)
+where
+    I: i2c::Write,
+    I::Error: core::fmt::Debug,
+{
+    i2c.write(ADDRESS, &[register, value]).unwrap();
 }
