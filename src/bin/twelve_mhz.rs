6f9156cb8b84b6aad0eca7b76c2f720be38874b2
Matt Mullins <mmullins@mmlx.us>
2021-01-23 16:52:10 -0800

    twelve_mhz: actually output 12MHz
    This uses 167.5MHz as a hack -- stm32f4xx-hal tries to choose the
    largest PLL input it can get away with, but since the PLL output divider
    can only go up to 7, we need a 1MHz PLL source clock.
    
    Technically this is still running the PLL's VCO at 60MHz, which does not
    meet the 100MHz minimum value.  But it seems to be outputting 12.0007MHz
    per the scope's measurement, though the low half of the clock cycle is a
    bit shorter than desired yet.

--- src/bin/twelve_mhz.rs
+++ src/bin/twelve_mhz.rs
@@ -12,14 +12,22 @@ fn main() -> ! {
     let peripherals = stm32f407g_disc::Peripherals::take().unwrap();
 
     let rcc = peripherals.RCC.constrain();
-    let _clocks = rcc.cfgr.use_hse(8.mhz()).sysclk(168.mhz()).freeze();
+    let _clocks = rcc.cfgr.use_hse(8.mhz()).sysclk(167_500.khz()).freeze();
 
     let portc = peripherals.GPIOC.split();
     let _mco2 = portc.pc9.into_alternate_af0();
 
     unsafe {
         let rcc = &*stm32::RCC::ptr();
-        rcc.cfgr.modify(|_r, w| w.mco2().hse());
+
+        rcc.plli2scfgr.write(|w| {
+            w.plli2sn().bits(60);
+            w.plli2sr().bits(5)
+        });
+        rcc.cr.modify(|_r, w| w.plli2son().set_bit());
+        while !rcc.cr.read().plli2srdy().bit() {}
+
+        rcc.cfgr.modify(|_r, w| w.mco2().plli2s());
     }
 
     loop {
