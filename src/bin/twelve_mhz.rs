d2c7dac160bcb4cdb7577319a73d38dc9361e875
Matt Mullins <mmullins@mmlx.us>
2021-01-23 22:38:12 -0800

    twelve_mhz: enable high-speed output drivers

--- src/bin/twelve_mhz.rs
+++ src/bin/twelve_mhz.rs
@@ -14,8 +14,28 @@ fn main() -> ! {
     let rcc = peripherals.RCC.constrain();
     let _clocks = rcc.cfgr.use_hse(8.mhz()).sysclk(167_500.khz()).freeze();
 
+    unsafe {
+        let rcc = &*stm32::RCC::ptr();
+        rcc.apb2enr.modify(|_r, w| w.syscfgen().set_bit());
+
+        // enable the compensation cell, as per RM0090:
+        //
+        // By default the I/O compensation cell is not used. However when the I/O output buffer
+        // speed is configured in 50 MHz or 100 MHz mode, it is recommended to use the compensation
+        // cell
+
+        // This is done by unsafely yanking the pointer out of peripherals, because the current SVD
+        // does not list CMPCR as a read-write register.q
+        let cmpcr = &peripherals.SYSCFG.cmpcr as *const _ as *mut u32;
+        core::ptr::write_volatile(cmpcr, 1);
+        while (core::ptr::read_volatile(cmpcr) & 0x100) == 0 {}
+    }
+
     let portc = peripherals.GPIOC.split();
-    let _mco2 = portc.pc9.into_alternate_af0();
+    let _mco2 = portc
+        .pc9
+        .into_alternate_af0()
+        .set_speed(stm32f407g_disc::gpio::Speed::VeryHigh);
 
     unsafe {
         let rcc = &*stm32::RCC::ptr();
